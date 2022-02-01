7cd1dda334ae736f1d1668ed3fc4e7b2c4faaf70
jovanaapostolovic <apostolovicjovana@gmail.com>
2022-02-01 11:52:59 +0100

    Projekat_Chat_Task3 nastavak_ Responsive domaci

--- Projekat_Chat/chat.js
+++ Projekat_Chat/chat.js
@@ -22,6 +22,42 @@ class Chatroom {
         return this._username;
     }
     
+    //dodavanje poruke
+    async addChat(mess) {
+        //dohvatanje trenutnog vremena koje nam je potrebno za timestamp (created_at)
+        let date = new Date();
+        //kreiranje dokumenta/objekta koji prosleđujem bazi podataka
+        let docChat = {
+            message: mess,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(date)
+        };
+
+        //da sačuvam dokument u db
+        let response = await this.chats.add(docChat);
+        return response; //vraćam promis i mogu za njega da kažem .then i .catch
+        
+    }
+
+    //metod koji prati promene u bazi i vraća poruke
+    getChats(callback){
+        this.chats.onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                //kada se desila promena u bazi ispisati "promena u bazi"
+                // console.log(change.type);
+                // if (change.type == "added") {
+                //     console.log("Promena u bazi!");
+                // }
+
+                //ispisati dokumente koji su dodati u bazu
+                if (change.type == "added") {
+                    // console.log(change.doc.data());
+                    callback(change.doc.data()); //prosleđivanje dokumenta na ispis(ispis realizujemo kada realizujemo callback)
+                }
+            });
+        });
+    }
 }
 
 export default Chatroom;
--- Projekat_Chat/script.js
+++ Projekat_Chat/script.js
@@ -21,7 +21,7 @@ import Chatroom from "./chat.js";
 
 let c1 = new Chatroom("js", "mirko72");
 let c2 = new Chatroom("general", "coka24");
-let c3 = new Chatroom("js", "sonja98");
+
 
 //test getera
 console.log(c1.username, c1.room);
@@ -32,3 +32,16 @@ console.log(c1.username);
 
 c1.room = "general";
 console.log(c1.room);
+
+c1.addChat("Hello");
+c2.addChat("HR trening!");
+
+let c3 = new Chatroom("js", "sonja98");
+//dodajemo poruku
+// c3.addChat("Pošaljite CV")
+//     .then( () => console.log("Uspešno dodat chat!"))
+//     .catch( err => console.log(err));
+
+c2.getChats(d => {
+    console.log(d);
+});
